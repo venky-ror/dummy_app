8c3eb53a63e53f37d87ee226e7ec5d0467e04efb
Venky Reddy <venkateswara@tonetag.com>
2022-07-10 14:29:13 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,100 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
+set :repo_url,        "git@github.com:venky-ror/Rspect-Test-Cases.git"
+set :application,     "dummy_app"
+set :user,            "ubuntu"
+set :puma_threads,    [4, 25]
+set :puma_workers,    0
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+set :deploy_to,       "/home/#{fetch(:user)}/ror/#{fetch(:application)}"
+set :puma_bind,       %w[tcp://0.0.0.0:9292 unix://#{shared_path}/tmp/sockets/#{fetch(:application)}_puma.sock]
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+## Defaults:
+set :scm,           :git
+set :branch,        :master
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :linked_files, fetch(:linked_files, []).push(
+  'config/database.yml',
+  'config/cable.yml',
+  'config/master.key',
+  'config/sidekiq.yml',
+  'config/storage.yml'
+)
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids',
+  'tmp/sockets', 'public/uploads', 'webapp/node_modules', 'webapp/dist', 'public/storage'
+)
+after 'deploy:published', 'custom:paranoia_setup' if ENV['MIGRATION_CHANGE'] || ENV['FIRST_DEPLOY']
+after 'deploy:published', 'pincode_setup:seed' if ENV['PINCODE_CHANGE'] || ENV['FIRST_DEPLOY']
+after 'deploy:published', 'deploy:webapp' if ENV['DEPLOY_WEB'] || ENV['FIRST_DEPLOY']
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :bundle_binstubs, nil
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
 
-# Default value for :pty is false
-# set :pty, true
+  before :start, :make_dirs
+end
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      puts "#{current_path}"
+      within "#{current_path}" do
+        with :rails_env => :production do
+          execute :rake, "initiate_library:dep"
+          execute :rake, "sidekiq:stop"
+          execute :rake, "sidekiq:restart"
+        end
+      end
+    end
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,22 +7,6 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
-
-# role-based syntax
-# ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
-
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
 # Configuration
 # =============
 # You can set any configuration variable like in config/deploy.rb
@@ -42,7 +26,7 @@
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
