f69a2d96e16aaa82aa2b151b6318c203ff4df3e7
Venky Reddy <venkateswara@tonetag.com>
2022-07-13 10:58:13 +0530

    storage disabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon #:local
+  #config.active_storage.service = :amazon #:local
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -82,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
  end 
