a7d71704533742c3ad7c45c7b59f9887928a63d5
Venky Reddy <venkateswara@tonetag.com>
2022-07-13 11:40:15 +0530

    puma issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,5 +88,5 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  # after  :finishing,    :restart
 end
