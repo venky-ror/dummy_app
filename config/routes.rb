7cc8d4fa4e5dfe006c7582578740f6dae58606e1
Venky Reddy <venkateswara@tonetag.com>
2022-07-20 22:34:59 +0530

    routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api/v1', defaults: {format: :json} do
+    devise_for :users, controllers: {sessions: 'api/v1/sessions', registrations: 'api/v1/registrations'}
+  end
   namespace :api do
     namespace :v1 do
-       devise_for :users, controllers: {sessions: 'api/v1/sessions', registrations: 'api/v1/registrations'}
        resources :articles do
        end
     end
